f967042c4ccfcd659910b433de467f9de603317a
hassam009 <hasideveloper@gmail.com>
2024-08-16 11:15:17 +0500

    now password in encrypted and tokens are used

--- server/Models/User-Models.js
+++ server/Models/User-Models.js
@@ -1,5 +1,6 @@
 const mongoose=require("mongoose");
-
+const bcrypt=require('bcryptjs')
+const jwt=require('jsonwebtoken')
 const userSchema=new mongoose.Schema({
     username:{
         type:String,
@@ -25,9 +26,46 @@ const userSchema=new mongoose.Schema({
 })
 
 //METHOD TO SECURE PASSWORD 
-userSchema.pre("save", async function(){
+userSchema.pre("save", async function(next){
 console.log("pre method", this)
+
+const user=this;
+if(!user.isModified("Password")){
+  next();
+}
+
+try{
+  const saltRound=await bcrypt.genSalt(10);
+  const hash_password= await bcrypt.hash(password, saltRound);
+  user.password=hash_password;
+}catch(error){
+  next(error);
+}
+
 })
+
+userSchema.methods.generateToken=async function(){
+  try{
+
+  }catch(error)
+  {
+    console.log(error)
+  }
+}
+//JSON WEB TOKENS
+userSchema.methods.generateTokens= async function(){
+try{
+return jwt.sign({
+  userId:this._id.toString(),
+  email:this.email,
+  isAdmin:this.isAdmin,
+},
+process.env.JWT_SECRET_KEY,
+);
+}catch(error){
+  console.log(error)
+}
+};
 
 //Define the Collection ot model name
 
--- server/controller/auth-controller.js
+++ server/controller/auth-controller.js
@@ -1,35 +1,34 @@
-
-
-const User=require('../Models/User-Models')
-const bcrypt=require('bcryptjs')
-
-
-
+const User = require('../Models/User-Models');
+const bcrypt = require('bcryptjs');
 
 const home = async (req, res) => {
     try {
-        res.status(200).send("I am home section come to you using auth controller");
+        res.status(200).send("I am home section coming to you using auth controller");
     } catch (error) {
         console.log("error");
     }
 };
 
-//TO SECURE PASSWORD USE BELOW METHODS IT USES THROUGH BCRYPT
-const saltRound=10;
-const hash_password= await bcrypt.hash(password, saltRound)
-//BUT THERE IS ALSO ANOTHER WAY WHICH IS PRE METHOD IT USED IN User-Models
-
 const register = async (req, res) => {
     try {
-     const {username, email, phone, password}=req.body;
-     const userExist= await User.findOne({email:email})
-     if(userExist){
-         return res.status(400).json({msg:"email already Exist"})
-     }
-     await User.create({username, email, phone, password });
-        res.status(200).json({ message: req.body });
+        const { username, email, phone, password } = req.body;
+        const userExist = await User.findOne({ email: email });
+
+        if (userExist) {
+            return res.status(400).json({ msg: "Email already exists" });
+        }
+
+        const userCreated = await User.create({ username, email, phone, password });
+        
+        // Generate a JWT token
+        
+        res.status(201).json({ 
+            msg: userCreated, 
+            token: await userCreated.generateToken(),
+            userId:usercreated._id.toString(),
+        });
     } catch (error) {
-        res.status(400).send({ msg: "page not found" });
+        res.status(500).json({ msg: "Internal Server Error" });
     }
 };
 
